db69e79a923bd3ad95101df1904ea5a57cfef7e4
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-04-27 23:47:40 +0200

    feat: added simple player struct

---
+++ src/include/LiFM-Data/Player.h
@@ -0,0 +1,20 @@
+//
+// Created by Elias Aggergaard Larsen on 27/04/2025.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+
+
+struct Player
+{
+  int playerID;
+  int clubID;
+  int shooting;
+  int passing;
+};
+
+
+
+#endif //PLAYER_H
