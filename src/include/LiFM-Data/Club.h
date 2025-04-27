678f51db52b24566d2d791868fc9603d26bad554
Elias Aggergaard Larsen <s235125@dtu.dk>
2025-04-27 23:51:11 +0200

    feat: added simple club struct

---
+++ src/include/LiFM-Data/Club.h
@@ -0,0 +1,16 @@
+//
+// Created by Elias Aggergaard Larsen on 27/04/2025.
+//
+
+#ifndef CLUB_H
+#define CLUB_H
+#include <vector>
+
+struct Club
+{
+  std::vector<int> playerIDs;
+  int leagueID;
+  int moneyAmount;
+};
+
+#endif //CLUB_H
